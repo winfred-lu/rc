544d46e0c7ef39470837bb1e48cc4290986daa5b
Winfred Lu <winfred.lu@gmail.com>
2011-10-25 10:13:15 +0800

    Common org-mode settings

--- emacs.el
+++ emacs.el
@@ -25,6 +25,14 @@
         (self-insert-command arg)))
 (define-key viper-insert-basic-map (kbd "j") 'viper-escape-if-next-char-is-j)
 
+;;; org mode
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-files (list "~/org/work.org"
+                             "~/org/home.org"))
 
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
