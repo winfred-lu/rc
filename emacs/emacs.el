742a3391724268946a27d57b4e405511d38cab86
Winfred Lu <winfred.lu@gmail.com>
2011-12-19 15:09:43 +0800

    Use evil instead ov viper (also adjust jj action). Customized variable: ediff split horizontally, show parens, and follow symbolic links.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -4,28 +4,58 @@
 (add-to-list 'load-path "color-theme.el")
 (require 'color-theme)
 (color-theme-initialize)
-(eval-after-load "color-theme" '(color-theme-clarity))
+(eval-after-load "color-theme" '(color-theme-ld-dark))
 
 ;;; viper
-(setq viper-mode t)
-(require 'viper)
+;(setq viper-mode t)
+;(require 'viper)
 
 ;;; imap jj to ESC
-(defun viper-escape-if-next-char (c)
-  (self-insert-command 1)
-    (let ((next-key (read-event)))
-      (if (= c next-key)
-        (progn
-          (delete-backward-char 1)
-            (viper-mode))
-            (setq unread-command-events (list next-key)))))
-(defun viper-escape-if-next-char-is-j (arg)
-  (interactive "p")
-    (if (= arg 1)
-      (viper-escape-if-next-char ?j)
-        (self-insert-command arg)))
-(define-key viper-insert-basic-map (kbd "j") 'viper-escape-if-next-char-is-j)
+;(defun viper-escape-if-next-char (c)
+;  (self-insert-command 1)
+;    (let ((next-key (read-event)))
+;      (if (= c next-key)
+;        (progn
+;          (delete-backward-char 1)
+;            (viper-mode))
+;            (setq unread-command-events (list next-key)))))
+;(defun viper-escape-if-next-char-is-j (arg)
+;  (interactive "p")
+;    (if (= arg 1)
+;      (viper-escape-if-next-char ?j)
+;        (self-insert-command arg)))
+;(define-key viper-insert-basic-map (kbd "j") 'viper-escape-if-next-char-is-j)
 
+;;; Evil
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+(evil-define-command cofi/maybe-exit ()
+  :repeat change
+  (interactive)
+  (let ((modified (buffer-modified-p)))
+    (insert "j")
+    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
+               nil 0.5)))
+      (cond
+       ((null evt) (message ""))
+       ((and (integerp evt) (char-equal evt ?j))
+    (delete-char -1)
+    (set-buffer-modified-p modified)
+    (push 'escape unread-command-events))
+       (t (setq unread-command-events (append unread-command-events
+                          (list evt))))))))
+
+; make evil work for org-mode!
+(define-key evil-normal-state-map "O" (lambda ()
+                     (interactive)
+                     (end-of-line)
+                     (org-insert-heading)
+                     (evil-append nil)
+                     ))
+ 
 ;;; org mode
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
@@ -50,7 +80,10 @@
  '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
  '(column-number-mode t)
- '(menu-bar-mode nil))
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(menu-bar-mode nil)
+ '(show-paren-mode t)
+ '(vc-follow-symlinks nil))
 (make-directory "~/.emacs.d/autosaves/" t)
 
 (custom-set-faces
@@ -59,3 +92,4 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+
