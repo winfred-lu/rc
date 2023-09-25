2c616cec465781a917691508447c6296054e3bd9
Winfred Lu <winfred.lu@gmail.com>
2023-09-25 13:26:59 +0800

    Insert '(' after select function or method item.

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -27,15 +27,13 @@ local M = {
   config = function(_, opts)
     require("nvim-autopairs").setup(opts)
 
-    --- setup for cmp
-    --[[
+    -- insert '(' after select function or method item
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    local cmp_status_ok, cmp = pcall(require, "cmp")
-    if not cmp_status_ok then
+    local ok, cmp = pcall(require, "cmp")
+    if not ok then
       return
     end
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    ]]
   end,
 }
 
